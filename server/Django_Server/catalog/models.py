4ac3b85b970134022dc646648880431f277e7eb8
Eugene Moshchyn <eugene.moschyn@gmail.com>
2020-08-17 04:11:49 -0700

    Added SQL Models

--- server/Django_Server/catalog/models.py
+++ server/Django_Server/catalog/models.py
@@ -1,3 +1,76 @@
 from django.db import models
 
-# Create your models here.
+#COMES FROM scrapper/scraperHandler.py
+
+
+##------------------Fundamental Information------------------##
+class Region(models.Model):
+    name = models.CharField(max_length = 20, unique = True)
+
+
+class Country(models.Model):
+    region = models.ForeignKey(Region, on_delete = models.CASCADE, unique = True)
+    name   = models.CharField(max_length = 100, unique = True)
+
+
+class Category(models.Model):
+    country = models.ForeignKey(Country, on_delete = models.CASCADE, unique = True)
+    name    = models.CharField(max_length = 255, unique = True)
+
+
+class Collection(models.Model):
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, unique = True)
+    name     = models.CharField(max_length = 255, unique = True)
+
+
+
+##------------------MintedBy Information------------------##
+class MintedBy(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+
+
+class Sculptor(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+
+
+
+##----------------Base Information----------------##
+class Material(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+
+
+class Quality(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+
+
+
+##-----------Characteristics Information-----------##
+class Shape(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+
+
+class SizeUnits(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+
+
+class WeightUnits(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+
+
+##---------------Coin Information---------------##
+class Coin(models.Model):
+    collection = models.ForeignKey(Collection, on_delete = models.CASCADE, unique = True)
+    name       = models.CharField(max_length = 255, unique = True)
+    year       = models.IntegerField()
+
+    mintedBy   = models.ForeignKey(MintedBy, on_delete = models.SET_NULL)
+
+
+class CoinStyles(models.Model):
+    coin     = models.ForeignKey(Coin, on_delete = models.CASCADE, unique = True)
+    material = models.ForeignKey(Material, on_delete = models.SET_NULL, unique = True)
+    standard = models.CharField(max_length = 30, unique = True)
