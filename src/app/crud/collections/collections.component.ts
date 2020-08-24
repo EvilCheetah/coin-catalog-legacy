00fb0ec423e040c4755893d069c8dc650d4287fa
ivan <ivan.pilipchuk@tut.by>
2020-08-25 00:36:48 +0300

    Added logic for collections component

--- src/app/crud/collections/collections.component.ts
+++ src/app/crud/collections/collections.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import {CollectionsService} from './collections.service';
+import {Collection} from 'src/app/classes/collection'
+import {Category} from 'src/app/classes/category';
 
 @Component({
   selector: 'app-collections',
@@ -7,9 +11,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:CollectionsService) { }
+
+  collections:Collection[]=[{id:1, name:"Collection1", category:1}, {id:2, name:"Collection2", category:2}];
+
+  categories:Category[]=[{id:1, name:"Russia", country:1}, {id:2, name:"Belarus", country:2}];
+
+  categoryControl= new FormControl('',[Validators.required, Validators.minLength(1)]);
+
+  nameControl = new FormControl('',[Validators.required, Validators.minLength(1)]);
+
+  selectedCollection:Collection = new Collection();
+
+  createMode:boolean=false;
 
   ngOnInit(): void {
+    //this.loadCountries();
+    //this.loadRegions();
+  }
+  loadCollection(){
+    this.service.getAllCollections().subscribe((data)=>this.collections=data);
+  }
+  loadCategories(){
+    this.service.getAllCategories().subscribe((data)=>this.categories=data);
+  }
+  changeCreateMode(){
+    this.cancelEdit();
+    this.createMode = !this.createMode;
+    this.categoryControl= new FormControl('',[Validators.required, Validators.minLength(1)]);
+    this.nameControl = new FormControl('',[Validators.required, Validators.minLength(1)]);
   }
 
+  getCategoryName(id:number):string{
+    return this.categories.find(category=>category.id==id).name;
+  }
+
+  selectCollection(collection:Collection){
+    this.selectedCollection=collection;
+    this.nameControl.setValue(collection.name);
+    this.categoryControl.setValue(collection.category);
+  }
+
+  cancelEdit(){
+    this.selectedCollection = new Collection();
+    this.categoryControl= new FormControl('',[Validators.required, Validators.minLength(1)]);
+    this.nameControl = new FormControl('',[Validators.required, Validators.minLength(1)]);
+  }
+
+  deleteCollection(id:number){
+    this.service.deleteCollection(id).subscribe(()=>this.loadCollection());
+  }
+
+  createCollection(){
+    let collection = new Collection();
+    collection.name = this.nameControl.value;
+    collection.category = this.categoryControl.value;
+    this.service.createCollection(collection).subscribe(()=>
+    {
+      this.loadCollection();
+      this.changeCreateMode();
+    })
+  }
+
+  saveEdit(){
+    let collection = new Collection();
+    collection.id = this.selectedCollection.id;
+    collection.name = this.nameControl.value;
+    collection.category = this.categoryControl.value;
+    this.service.editCollection(collection).subscribe(()=>
+    {
+      this.loadCollection();
+      this.cancelEdit();
+    })
+  }
 }
+
--- src/app/crud/collections/collections.service.ts
+++ src/app/crud/collections/collections.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {Collection} from 'src/app/classes/collection'
+import {Category} from 'src/app/classes/category';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CollectionsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getAllCategories():Observable<Category[]>{
+    return this.http.get<Category[]>("");
+  }
+  public getAllCollections():Observable<Collection[]>{
+    return this.http.get<Collection[]>("");
+  }
+
+  public editCollection(collection:Collection){
+    return this.http.put("",collection);
+  }
+
+  public createCollection(collection:Collection){
+    return this.http.post("",collection);
+  }
+  
+  public deleteCollection(id:number){
+    var param:HttpParams=new HttpParams();
+    param.set("id",id.toString())
+    return this.http.delete("", {params:param});
+  }
 }
