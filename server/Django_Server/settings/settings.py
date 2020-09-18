477f8d5db91bbd315371a57b4ee865d6af4de9a4
Eugene Moshchyn <eugene.moschyn@gmail.com>
2020-09-18 16:32:43 -0700

    Added CORS to the project for compatability with Angular.

--- server/Django_Server/settings/settings.py
+++ server/Django_Server/settings/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     #REST FRAMEWORK Libraries
     'rest_framework',
     'django_filters',
+    'corsheaders',
 
     #Apps
     'coin_catalog',
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -131,3 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CORS_ORIGIN_WHITELIST = [
+    'localhost:4200',
+]
