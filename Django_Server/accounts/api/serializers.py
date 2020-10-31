1dc3533d3e2c9059cea1b35bc35912dde3fb8c8b
Eugene Moshchyn <eugene.moschyn@gmail.com>
2020-10-31 13:59:22 -0700

    Added ID field for accounts

--- Django_Server/accounts/api/serializers.py
+++ Django_Server/accounts/api/serializers.py
@@ -39,6 +39,6 @@ class RegistrationSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['email',       'username',
+        fields = ['id',          'email',    'username',
                   'is_active',   'is_staff', 'is_admin', 'is_superuser',
                   'date_joined', 'last_login']
