14c992ad2b112bdd5ac70ecde8d332d012f76c62
Eugene M <eugene.moschyn@gmail.com>
2020-11-28 17:37:44 -0800

    Removed base registration in order to implement Djoser.

--- Django_Server/accounts/api/serializers.py
+++ Django_Server/accounts/api/serializers.py
@@ -1,39 +1,6 @@
 from rest_framework import serializers
 
 from accounts.models import Account
-from accounts.services import serializer_logic as Logic
-
-
-class RegistrationSerializer(serializers.ModelSerializer):
-    confirmation_password = serializers.CharField(
-        style      = {'input_type': 'password'},
-        write_only = True
-    )
-
-    class Meta:
-        model  = Account
-        fields = ['email', 'username', 'password', 'confirmation_password']
-        extra_kwargs = {
-            'password': {
-                'write_only': True
-            }
-        }
-
-    def save(self):
-        account = Account(
-            email    = self.validated_data['email'],
-            username = self.validated_data['username']
-        )
-
-        password              = self.validated_data['password']
-        confirmation_password = self.validated_data['confirmation_password']
-
-        Logic.raise_error_if_passwords_do_not_match(password, confirmation_password)
-
-        account.set_password(password)
-        account.save()
-
-        return account
 
 
 class UserSerializer(serializers.ModelSerializer):
--- Django_Server/accounts/api/views.py
+++ Django_Server/accounts/api/views.py
@@ -8,21 +8,6 @@ from accounts.api import serializers as AccountSerializer
 from accounts.services import view_logic as ViewLogic
 
 
-@api_view(['POST'])
-def registration(request):
-    serializer = AccountSerializer.RegistrationSerializer(data = request.data)
-    data = {}
-
-    if serializer.is_valid():
-        account = serializer.save()
-        data = ViewLogic.registration_success_response_data(account)
-
-    else:
-        data = serializer.errors
-
-    return Response(data)
-
-
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = AccountModel.Account.objects.all()
     serializer_class = AccountSerializer.UserSerializer
