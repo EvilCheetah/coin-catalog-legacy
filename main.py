b4d7260f6b95fc8be5603d409319f45912d21f8a
Eugene M <eugene.moschyn@gmail.com>
2020-11-30 20:28:58 -0800

    Changed main for Russia catalog

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-'''
+from scrapper.Russia import scrapper
 
-	Author:		Eugene Moshchyn
-	Description:
-		- int main()
-
-'''
-
-from scraperHandler import DB_Handler
-#from scrapers.BY import BY_Scraper
 
 if __name__ == '__main__':
 	try:
-		DB_Handler()
-		#BY_Scraper().getCoins()
+		scrapper.main()
 
 	except KeyboardInterrupt:
-		print("Program Terminated")
+		print('Program has been terminated...')
