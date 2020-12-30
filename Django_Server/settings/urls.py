d69a8334cd45b7a9c115a169887959ce33167fe0
Eugene M <eugene.moschyn@gmail.com>
2020-12-30 14:51:13 -0800

    Added authentication.

--- Django_Server/settings/urls.py
+++ Django_Server/settings/urls.py
@@ -1,9 +1,23 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/',       admin.site.urls),
+    #path('api/account/', include('accounts.urls')),
     path('api/catalog/', include('coin_catalog.urls')),
-    path('api/accounts/', include('accounts.api.urls'))
+
+    path('auth/',        include('djoser.urls')),
+    path('auth/',        include('djoser.urls.jwt')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
