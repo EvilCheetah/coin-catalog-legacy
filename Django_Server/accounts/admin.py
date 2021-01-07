a6ef7e8b734211fb7c646d87d630efa72332ba94
Eugene M <eugene.moschyn@gmail.com>
2021-01-06 17:06:00 -0800

    Added Accounts to admin panel

--- Django_Server/accounts/admin.py
+++ Django_Server/accounts/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from accounts.models import Account
 
 
-class AccountAdmin(UserAdmin):
-    list_display    = ['email', 'username', 'date_joined', 'last_login', 'is_active', 'is_staff', 'is_admin']
-    search_fields   = ['email', 'username']
-    readonly_fields = ['date_joined', 'last_login']
-
-    filter_horizontal = []
-    list_filter       = []
-    fieldsets         = []
+class AccountAdmin(admin.ModelAdmin):
+    list_display  = ['email', 'username', 'date_joined', 'last_login',
+                     'is_active', 'is_staff', 'is_admin', 'is_superuser']
+    search_fields = ['email', 'username']
 
 
 admin.site.register(Account, AccountAdmin)
