18ae258be26d2158c9ee93484e236ac7b07db588
Eugene M <eugene.moschyn@gmail.com>
2021-01-11 17:45:31 -0800

    Edited max and min values. Added docstrings

--- Django_Server/coin_catalog/api/pagination.py
+++ Django_Server/coin_catalog/api/pagination.py
@@ -10,6 +10,21 @@ class StandardResultsSetPagination(PageNumberPagination):
 
 
 class InformationResultsSetPagination(PageNumberPagination):
-    page_size               = 25
-    max_page_size           = 1000
+    """
+        Default pagination class for the ViewSets that
+        don't "render" additional information
+    """
+    page_size               = 50
+    max_page_size           = 10000
+    page_size_query_param   = 'page_size'
+
+
+class FullInformationResultsSetPagination(PageNumberPagination):
+    """
+        Pagination class for the 'FullInfoCoinViewSet'
+        limits the number of coin in order to ease the
+        server
+    """
+    page_size               = 10
+    max_page_size           = 100
     page_size_query_param   = 'page_size'
