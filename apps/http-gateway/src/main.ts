54e2dc3035d76bb4271301b60081579c9ae525c6
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-03-03 20:29:21 -0800

    init(): project structure

---
+++ apps/http-gateway/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { HttpGatewayModule } from './http-gateway.module';
+
+
+async function bootstrap()
+{
+    const app = await NestFactory.create(HttpGatewayModule);
+    await app.listen(3000);
+}
+
+
+bootstrap();
