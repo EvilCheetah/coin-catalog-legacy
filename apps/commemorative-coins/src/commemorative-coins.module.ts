1ccdd0cbbb1e3983b7417b2543f36afda406b5fd
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-03-03 20:36:15 -0800

    add(): config module initialization

--- apps/commemorative-coins/src/commemorative-coins.module.ts
+++ apps/commemorative-coins/src/commemorative-coins.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GeolocationModule } from './geo-location/geo-location.module';
 
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: '../environment/.env'
+        }),
+
         GeolocationModule
     ]
 })
