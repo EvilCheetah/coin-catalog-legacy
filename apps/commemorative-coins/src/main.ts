718f9aacc52aa92c29fdbea8872f32ed40a5a39d
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-03-09 15:00:43 -0800

    refactor(): to be a microservice

--- apps/commemorative-coins/src/main.ts
+++ apps/commemorative-coins/src/main.ts
@@ -1,3 +1,5 @@
+import { ServiceName } from '@/constants';
+import { RabbitMQService } from '@/modules';
 import { NestFactory } from '@nestjs/core';
 import { CommemorativeCoinsModule } from './commemorative-coins.module';
 
@@ -5,7 +7,13 @@ import { CommemorativeCoinsModule } from './commemorative-coins.module';
 async function bootstrap()
 {
     const app = await NestFactory.create(CommemorativeCoinsModule);
-    await app.listen(3000);
+
+    const rmq_microservice = app.get<RabbitMQService>(RabbitMQService);
+    app.connectMicroservice(
+        rmq_microservice.get_config(ServiceName.CommemorativeCoinsService)
+    );
+
+    await app.startAllMicroservices();
 }
 
 
