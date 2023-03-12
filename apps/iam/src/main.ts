cc1cf0366acb92416e1371b0899a539cce259ae2
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-03-11 21:16:03 -0800

    add(): connect

--- apps/iam/src/main.ts
+++ apps/iam/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { IamModule } from './iam.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(IamModule);
-  await app.listen(3000);
+import { Services } from '@/constants';
+import { IamModule } from './iam.module';
+import { RabbitMQService } from '@/modules';
+
+
+async function bootstrap()
+{
+    const app = await NestFactory.create(IamModule);
+
+    const rmq_microservice = app.get<RabbitMQService>(RabbitMQService);
+    app.connectMicroservice(
+        rmq_microservice.get_config(Services.IamService)
+    );
+
+    await app.startAllMicroservices();
 }
+
+
 bootstrap();
