6802104fbce058e21a39ce70373d7aafa0663a6e
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-03-12 21:50:59 -0700

    add(): env validation

--- apps/commemorative-coins/src/commemorative-coins.module.ts
+++ apps/commemorative-coins/src/commemorative-coins.module.ts
@@ -1,13 +1,15 @@
-import { RabbitMQModule } from '@/modules';
 import { Module } from '@nestjs/common';
+
+import { RabbitMQModule } from '@/modules';
 import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './config/validation.schema';
 import { GeolocationModule } from './geo-location/geo-location.module';
 
 
 @Module({
     imports: [
         ConfigModule.forRoot({
-            envFilePath: './environment/.env',
+            validationSchema,
             expandVariables: true
         }),
         RabbitMQModule,
