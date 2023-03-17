a537ea51578661ae4e65c74cc6373a7a1f551e3d
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-03-17 04:59:06 -0700

    add(): region repository used to wrap logic for mikro-orm

--- apps/commemorative-coins/src/geo-location/region/region.module.ts
+++ apps/commemorative-coins/src/geo-location/region/region.module.ts
@@ -4,11 +4,12 @@ import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Region } from "./entities/region.entity";
 import { RegionService } from "./region.service";
 import { RegionController } from "./region.controller";
+import { RegionRepository } from "./region.repository";
 
 
 @Module({
     imports:     [MikroOrmModule.forFeature([ Region ])],
     controllers: [RegionController],
-    providers:   [RegionService]
+    providers:   [RegionService, RegionRepository]
 })
 export class RegionModule {}
---
+++ apps/commemorative-coins/src/geo-location/region/region.repository.ts
@@ -0,0 +1,60 @@
+import { Injectable } from "@nestjs/common";
+import { EntityRepository, wrap } from "@mikro-orm/core";
+import { InjectRepository } from "@mikro-orm/nestjs";
+
+import { Region } from "./entities/region.entity";
+import { CreateRegionDTO } from "./dto/create-region.dto";
+import { RegionNotFoundException } from "./errors/region-not-found.exception";
+import { UpdateRegionDTO } from "./dto/update-region.dto";
+
+
+@Injectable()
+export class RegionRepository
+{
+    constructor(
+        @InjectRepository(Region)
+        private readonly regionRepository: EntityRepository<Region>
+    ) {}
+
+
+    async create(createRegionDTO: CreateRegionDTO): Promise<Region>
+    {
+        const region = this.regionRepository.create(createRegionDTO);
+
+        await this.regionRepository.persistAndFlush(region);
+
+        return region;
+    }
+
+    find_all(): Promise<Region[]>
+    {
+        return this.regionRepository.findAll();
+    }
+
+    async find_one(region_id: number): Promise<Region>
+    {
+        const region = await this.regionRepository.findOne({ region_id });
+
+        if ( !region )
+            throw new RegionNotFoundException(region_id);
+        
+        return region;
+    }
+
+    async update(region_id: number, updateRegionDTO: UpdateRegionDTO): Promise<Region>
+    {
+        const region = await this.find_one(region_id);
+
+        wrap(region).assign(updateRegionDTO);
+        await this.regionRepository.persistAndFlush(region);
+
+        return region;
+    }
+
+    async remove(region_id: number): Promise<void>
+    {
+        const region = await this.find_one(region_id);
+
+        await this.regionRepository.removeAndFlush(region);
+    }
+}
