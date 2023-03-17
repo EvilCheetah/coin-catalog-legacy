13548d43aa157b935320fb3d68d963cb27e7afed
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-03-17 04:45:45 -0700

    add(): region repository from mikro orm

--- apps/commemorative-coins/src/geo-location/region/region.module.ts
+++ apps/commemorative-coins/src/geo-location/region/region.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
+import { MikroOrmModule } from "@mikro-orm/nestjs";
 
+import { Region } from "./entities/region.entity";
 import { RegionService } from "./region.service";
 import { RegionController } from "./region.controller";
 
 
 @Module({
+    imports:     [MikroOrmModule.forFeature([ Region ])],
     controllers: [RegionController],
     providers:   [RegionService]
 })
