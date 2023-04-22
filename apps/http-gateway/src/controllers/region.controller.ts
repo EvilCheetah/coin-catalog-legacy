a22e052589eaaaacd9df2e5d267147bafeb63a68
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-04-22 02:31:58 -0700

    remove(): return type from create method

--- apps/http-gateway/src/controllers/region.controller.ts
+++ apps/http-gateway/src/controllers/region.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Inject, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Inject, Logger, Patch, Post } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
+import { lastValueFrom } from 'rxjs';
 
 import { Services } from '@/constants';
 import { RegionCreate } from '@/contracts';
@@ -8,9 +9,11 @@ import { RegionCreate } from '@/contracts';
 @Controller('regions')
 export class RegionController
 {
+    private readonly logger = new Logger(RegionController.name)
+    
     constructor(
         @Inject(Services.CommemorativeCoinsService)
-        private readonly rabbitMQ: ClientProxy
+        private readonly rabbitMQ: ClientProxy,
     ) {}
 
     @Post()
@@ -19,7 +22,7 @@ export class RegionController
         data: RegionCreate.Request
     )
     {
-        return this.rabbitMQ.send<RegionCreate.Response, RegionCreate.Request>(
+        return this.rabbitMQ.send<RegionCreate.Response>(
             RegionCreate.topic, data
         );
     }
